05f52ed7cf3030a30eda00097621c1fba6109e1c
Balogun Temitope <85529178+Temitope-cloud@users.noreply.github.com>
2025-05-01 21:00:05 +0100

    Initial Commit

---
+++ src/context/ThemeContext.tsx
@@ -0,0 +1,55 @@
+"use client";
+import {
+  createContext,
+  ReactNode,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+
+type Theme = "light" | "dark";
+
+interface ThemeContextType {
+  theme: Theme;
+  toggleTheme: () => void;
+}
+
+const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
+
+export const useTheme = (): ThemeContextType => {
+  const context = useContext(ThemeContext);
+  if (!context) {
+    throw new Error("useTheme must be within a theme provider");
+  }
+  return context;
+};
+
+interface ThemeProviderProps {
+  children: ReactNode;
+}
+export const ThemeProvider = ({ children }: ThemeProviderProps) => {
+  const [theme, setTheme] = useState<Theme>(() => {
+    const savedTheme =
+      typeof window !== "undefined" ? localStorage.getItem("theme") : null;
+    return savedTheme === "dark" || savedTheme === "light"
+      ? savedTheme
+      : "light";
+  });
+
+  const toggleTheme = () => {
+    const newTheme: Theme = theme === "dark" ? "light" : "dark";
+    setTheme(newTheme);
+    localStorage.setItem("theme", newTheme);
+  };
+
+  useEffect(() => {
+    document.body.className = "";
+    document.body.classList.add(theme);
+  }, [theme]);
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
