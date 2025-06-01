7fbaf0aa0d4db0779775f0518cf85e9a1db8ed95
Balogun Temitope <topebalogun14@gmail.com>
2025-06-01 23:57:29 +0100

    Update Conference Invitation

--- conference-invitation/src/app/page.tsx
+++ conference-invitation/src/app/page.tsx
@@ -1,9 +1,12 @@
 "use client";
-import React, { useState } from "react";
+import React, { ChangeEvent, useState } from "react";
 
 interface UserDetails {
+  id: string;
   name: string;
   email: string;
+  image: string;
+  ticketType: string;
 }
 
 const MainPage = () => {
@@ -11,6 +14,31 @@ const MainPage = () => {
   const [email, setEmail] = useState("");
   const [newUserId, setNewUserId] = useState<string | null>("");
   const [showUserId, setShowUserId] = useState<boolean | null>(false);
+  type TicketType = "Student" | "Regular" | "VIP";
+  const [ticketType, setTicketType] = useState<TicketType>("Student");
+  const [fileSelctedName, setFileSelctedName] = useState("");
+  const [base64, setBase64] = useState("");
+
+  const ticketTypes = ["Student", "Regular", "VIP"];
+
+  const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+
+    if (!file) return;
+    setFileSelctedName(file.name);
+
+    const reader = new FileReader();
+
+    reader.onload = () => {
+      const base64String = reader.result as string;
+      setBase64(base64String);
+      // localStorage.setItem("userImage", base64String);
+      console.log("base 64 Image string saved");
+    };
+
+    reader.readAsDataURL(file);
+  };
+  // const savedImage = localStorage.getItem("userImage");
 
   const handleSubmit = () => {
     const acceptedEmail = email.toLowerCase();
@@ -26,10 +54,12 @@ const MainPage = () => {
       return;
     }
 
-    const newUser: UserDetails & { id: string } = {
+    const newUser: UserDetails & { id: string; image: string } = {
       id: Date.now().toString(),
       name: name.toLowerCase(),
       email: acceptedEmail,
+      image: base64,
+      ticketType: ticketType,
     };
 
     const updatedUser = [...existingUsers, newUser];
@@ -47,7 +77,7 @@ const MainPage = () => {
 
   return (
     <>
-      <div className="h-screen w-screen flex justify-center items-center bg-gray-50  text-gray-900">
+      <div className="h-screen w-screen flex justify-center items-center bg-gradient-to-br from-green-100 to-indigo-200 text-gray-800">
         <div className="flex flex-col shadow p-5 gap-3 border rounded w-[40%]">
           <input
             type="text"
@@ -56,22 +86,60 @@ const MainPage = () => {
             value={name}
             onChange={(e) => setName(e.target.value)}
             placeholder="Name"
-            className=" shadow border rounded p-2 "
+            className=" shadow border rounded p-2 outline-none"
           />
           <input
             type="email"
-            placeholder="email"
+            placeholder="Email Address"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
-            className=" shadow border rounded p-2"
+            className=" shadow border rounded p-2 outline-none"
           />
+          <select
+            name=""
+            id=""
+            value={ticketType}
+            onChange={(e) => setTicketType(e.target.value as TicketType)}
+            className=" shadow border rounded p-2 outline-none"
+          >
+            <option className="hidden"> Select ticket type </option>
+            {ticketTypes.map((t, idx) => (
+              <option key={idx} value={t}>
+                {t}
+              </option>
+            ))}
+          </select>
+          <label
+            htmlFor="fileInput"
+            className="shadow border rounded p-2 outline-none  px-4 py-2  cursor-pointer"
+          >
+            {" "}
+            Choose File
+          </label>
+          <input
+            type="file"
+            name=""
+            id="fileInput"
+            className="hidden"
+            onChange={handleFileChange}
+          />
+
+          {/* {savedImage && <img src={savedImage} alt="" />} */}
+
+          {fileSelctedName && (
+            <p>
+              {" "}
+              File Selected:{" "}
+              <span className="font-bold"> {fileSelctedName} </span>{" "}
+            </p>
+          )}
           <button
             onClick={() =>
               name !== "" && email !== ""
                 ? handleSubmit()
                 : alert("The fields are empty")
             }
-            className="bg-slate-900 text-white py-2 rounded"
+            className="bg-slate-900 text-white py-2 rounded cursor-pointer"
           >
             Submit
           </button>
@@ -79,7 +147,13 @@ const MainPage = () => {
             <p>
               {" "}
               Your Link:{" "}
-              <a href=""> {`http://localhost:3000/${newUserId}`} </a>{" "}
+              <a
+                href={`http://localhost:3000/ticket/${newUserId}`}
+                target="_blank"
+              >
+                {" "}
+                {`http://localhost:3000/ticket/${newUserId}`}{" "}
+              </a>{" "}
             </p>
           )}
         </div>
--- conference-invitation/src/app/ticket/[id]/page.tsx
+++ conference-invitation/src/app/ticket/[id]/page.tsx
@@ -6,11 +6,16 @@ interface UserDetails {
   id: string;
   name: string;
   email: string;
+  image: string;
+  ticketType: string;
+  status: string;
 }
 
 const TicketPage = () => {
   const { id } = useParams();
   const [user, setUser] = useState<UserDetails | null>(null);
+  const [attendanceStatus, setAttendanceStatus] = useState<string>("");
+  const [storedUserStatus, setStoredUserStatus] = useState<boolean | null>();
 
   useEffect(() => {
     const stored = localStorage.getItem("userDetails");
@@ -18,14 +23,100 @@ const TicketPage = () => {
     const foundUser = users.find((user) => user.id === id);
     setUser(foundUser || null);
   }, []);
+
+  const handleUserStatus = (status: "accept" | "decline") => {
+    const stored = localStorage.getItem("userDetails");
+    const users: UserDetails[] = stored ? JSON.parse(stored) : [];
+
+    const userExist = users?.find((existing) => existing.id === id);
+
+    if (userExist) {
+      const newStatus = status === "accept" ? "Accepted" : "Rejected";
+      userExist.status = newStatus;
+      // setUser({ ...userExist });
+      setAttendanceStatus(newStatus);
+      setStoredUserStatus(newStatus === "Accepted" ? true : false);
+      const updatedUser = users.map((user) =>
+        user.id === id ? userExist : user
+      );
+      localStorage.setItem("userDetails", JSON.stringify(updatedUser));
+      console.log(updatedUser);
+    }
+  };
+
+  useEffect(() => {
+    const stored = localStorage.getItem("userDetails");
+    const users: UserDetails[] = stored ? JSON.parse(stored) : [];
+    const foundUser = users.find((user) => user.id === id);
+
+    if (foundUser) {
+      if (foundUser.status === "Accepted") {
+        setStoredUserStatus(true);
+        setAttendanceStatus("Accepted");
+      } else if (foundUser.status === "Rejected") {
+        setStoredUserStatus(false);
+        setAttendanceStatus("Rejected");
+      } else {
+        setStoredUserStatus(null);
+      }
+    } else {
+      setStoredUserStatus(null);
+    }
+
+    console.log(foundUser);
+  }, []);
+
+  useEffect(() => {
+    console.log(attendanceStatus);
+  }, [attendanceStatus]);
   return (
     <>
-      <div className="h-screen w-screen flex justify-center items-center bg-gray-50  text-gray-900">
-        <div>
-          <p>{user?.id}</p>
-          <p>{user?.name}</p>
-          <p>{user?.email}</p>
-        </div>
+      <div className="h-screen w-screen flex justify-center items-center bg-gradient-to-br from-green-100 to-indigo-200 text-gray-800">
+        {user ? (
+          <div className="bg-white rounded-lg shadow-lg p-6 w-80 text-center">
+            <h1 className="text-2xl font-bold text-green-600 mb-4">
+              🎟 Event Ticket
+            </h1>
+            <div className="flex flex-col items-center gap-2">
+              <img
+                src={user?.image}
+                className="w-24 h-24 rounded-full border-4 border-green-500 object-cover object-top"
+                alt="User"
+              />
+              <p className="font-semibold capitalize">{user?.name}</p>
+              <p className="text-sm text-gray-500">{user?.email}</p>
+              <p className="mt-2">
+                <span className="font-bold">Ticket Type:</span>{" "}
+                {user?.ticketType}
+              </p>
+              <p className="text-xs text-gray-400">ID: {user?.id}</p>
+            </div>
+            {storedUserStatus === null ? (
+              <div className="mt-4 flex gap-2 justify-center">
+                <button
+                  onClick={() => handleUserStatus("accept")}
+                  className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600"
+                >
+                  Accept User
+                </button>
+                <button
+                  onClick={() => handleUserStatus("decline")}
+                  className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600"
+                >
+                  Decline User
+                </button>
+              </div>
+            ) : (
+              <p>
+                {" "}
+                This guest has been{" "}
+                <span className="font-bold"> {attendanceStatus} </span>{" "}
+              </p>
+            )}
+          </div>
+        ) : (
+          "..."
+        )}
       </div>
     </>
   );
